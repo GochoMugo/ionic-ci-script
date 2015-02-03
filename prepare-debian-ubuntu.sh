3f77adc917d3220284e14f947c4df861f58edd89
Gocho Mugo <mugo@forfuture.co.ke>
2015-02-03 17:47:11 +0300

    Installing ADT with external script
    Installing the Android SDK Installer using the android-sdk-installer
    from https://github.com/GochoMugo/android-sdk-installer

--- prepare-debian-ubuntu.sh
+++ prepare-debian-ubuntu.sh
@@ -9,14 +9,12 @@
 # add repositories
 ionicCi_add_repos() {
   add-apt-repository --yes ppa:chris-lea/node.js
-  add-apt-repository --yes ppa:upubuntu-com/sdk
   apt-get update --yes
 }
 
 
 # install the required dependencies/tools
 ionicCi_install_requirements() {
-  apt-get install --yes openjdk-7-jdk
   apt-get install --yes python python-software-properties
   apt-get install --yes nodejs
   npm -g install cordova ionic bower gulp cordova-gen
@@ -27,8 +25,8 @@ ionicCi_install_requirements() {
 
 # install ADT
 ionicCi_install_adt() {
-  apt-get install --yes lib32z1 lib32stdc++6
-  apt-get install --yes android-sdk
+  wget https://raw.githubusercontent.com/GochoMugo/android-sdk-installer/master/android-sdk-installer.sh
+  sudo bash android-sdk-installer.sh --yes
 }
 
 
@@ -43,8 +41,7 @@ ionicCi_log() {
 # export environment variables
 ionicCi_export_vars() {
   ANDROID_TOOLS=$(dirname $(which android))
-  ANDROID_HOME=$(dirname ${ANDROID_TOOLS})
-  export ANDROID_HOME
+  export ANDROID_HOME=$(dirname ${ANDROID_TOOLS})
   export PATH=${ANDROID_HOME}/platform-tools:${PATH}
   export PATH=${ANDROID_TOOLS}:${PATH}
 }
